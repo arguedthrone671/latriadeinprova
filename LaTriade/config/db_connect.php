2c1665fb090f13a25813d5040149de6b05db04b8
arguedthrone671 <131705422+arguedthrone671@users.noreply.github.com>
2023-09-24 18:40:55 +0200

    Delete LaTriade directory

--- LaTriade/config/db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php 
-   $conn = mysqli_connect('localhost', 'Lorenzo', 'Lollofp', 'latriade');
-
-    //check connection
-    if(!$conn) {
-        echo 'connection error: ' . mysqli_connect_error();
-    }
-?>
